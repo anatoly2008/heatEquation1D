1172e3a10f8870e35eb6f2ffe73a9fe51e86792e
Michael Klamkin <klamike@users.noreply.github.com>
2017-08-02 14:29:22 -0400

    Add files via upload

--- dudt.m
+++ dudt.m
@@ -1,9 +1,10 @@
 %% Solving the Heat Equation in 1 Dimension via Finite Difference Method (Backwards Euler)
 % dudt function for use in ode45, relies on global A and b
 % By Michael Klamkin 2017
-
 function out = dudt( t, u )
-    global A  b    
-    out = (A*u + b);
+
+    global A b    
+    out = (A*u + b);    
+    
 end
 
--- heat1d.m
+++ heat1d.m
@@ -13,13 +13,13 @@ omega = c/dx.^2; % just to not type out k/dx.^2 5 times
 u_0 = 80; u_end = 20; %initial conditions
 %% Initialization 
 
-A = zeros(n); 
-for i = 1:n-1
+A = zeros(n-1); 
+for i = 1:n-2
     A(i,i) = -2*omega; %central diag
     A(i, i+1) = omega; %lower diag
     A(i+1, i) = omega; %upper diag
 end
-A = A(1:n-1, 1:n-1); %upper and lower diags should be n-1, center should be n. saves loop time
+A(n-1,n-1) = -2 * omega;
 
 b = zeros(n-1, 1);
 b(1) = omega * u_0; %initial conditions
@@ -31,9 +31,15 @@ u_0 = b / omega; %initial conditions
 [t, u] = ode45( @dudt, [0,3], u_0); % see help/doc for more info
 
 figure(1);
-%    subplot(1, 2, 1);
-        pcolor(u'); shading interp; set(gca,'Ydir','reverse'); %pcolor allows for interp shading style
-        colorbar; % display colorbar
-% 
-%     subplot(1, 2, 2);
-%     imagesc(u'); colorbar;  %to compare with imagesc
+%     subplot(1, 2, 1);
+        fig1 = pcolor(u'); shading interp; %colormap jet %set(gca,'Ydir','reverse'); %pcolor allows for interp shading style
+        cbar = colorbar; cbar.Label.String = 'Temperature'; % display colorbar
+        cbarpos = get(get(cbar,'YLabel'),'Position');
+        set(get(cbar,'YLabel'),'Position',[cbarpos(1) + 3, cbarpos(2) + 40, cbarpos(3)],'Rotation',-90, 'FontSize', 15)
+        title('1D Heat Equation'); % figure title
+        set(gca, 'FontName', 'Times New Roman'); set(gca, 'TitleFontSizeMultiplier', 1.25); set(gca, 'FontSize', 15); % figure styling
+        ylabel('Position on Rod'); % y axis label
+        xlabel('Time (s)'); % x axis label
+       
+%      subplot(1, 2, 2);
+%      imagesc(u'); colorbar;  %to compare with imagesc
