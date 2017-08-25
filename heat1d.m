0cc005c664d2ddca4d2658f35446c8328b5d5e52
Michael Klamkin <klamike@users.noreply.github.com>
2017-08-24 23:25:09 -0400

    Update heat1d.m
    added forward euler discretization

--- heat1d.m
+++ heat1d.m
@@ -1,4 +1,4 @@
-%% Solving the Heat Equation in 1 Dimension via Finite Difference Method (Backwards Euler)
+%% Solving the Heat Equation in 1 Dimension via Finite Difference Method
 % Dependent on the function dudt in dudt.m
 % By Michael Klamkin 2017
 clear all; close all;
@@ -10,33 +10,77 @@ n = 100; % number of points on the rod
 c = 0.1; % thermal diffusion constant
 dx = 1/n; % delta x
 omega = c/dx.^2; % just to not type out k/dx.^2 5 times
-u_0 = 80; u_end = 20; %initial conditions
+tstop = 3; %length of simulation
 %% Initialization 
 
 A = zeros(n-1); 
+
 for i = 1:n-2
     A(i,i) = -2*omega; %central diag
     A(i, i+1) = omega; %lower diag
     A(i+1, i) = omega; %upper diag
 end
+
 A(n-1,n-1) = -2 * omega;
 
-b = zeros(n-1, 1);
-b(1) = omega * u_0; %initial conditions
-b(end) = omega * u_end; %initial conditions
+for i = 1:n-1 %init cond
+    b(i,1) = omega*(10*sin(pi*i*0.01));
+end
 
 u_0 = b / omega; %initial conditions
 %% ode45 and plotting via imagesc
-
-[t, u] = ode45( @dudt, [0,3], u_0); % see help/doc for more info
+tic
+[t, u] = ode45( @dudt, [0,tstop], u_0); % see help/doc for more info
 
 figure(1);
-%     subplot(1, 2, 1);
-        fig1 = pcolor(u'); shading interp; %colormap jet %set(gca,'Ydir','reverse'); %pcolor allows for interp shading style
-        cbar = colorbar; cbar.Label.String = 'Temperature'; % display colorbar
+     subplot(1, 2, 1);
+        fig1 = pcolor(fliplr(u')); shading interp; %colormap jet %set(gca,'Ydir','reverse'); %pcolor allows for interp shading style
+        cbar = colorbar;
         cbarpos = get(get(cbar,'YLabel'),'Position');
         set(get(cbar,'YLabel'),'Position',[cbarpos(1) + 3, cbarpos(2) + 40, cbarpos(3)],'Rotation',-90, 'FontSize', 15);
-        title('1D Heat Equation'); % figure title
+        title('1D Heat Equation - ode45()'); % figure title
         set(gca, 'FontName', 'Times New Roman'); set(gca, 'TitleFontSizeMultiplier', 1.25); set(gca, 'FontSize', 15); % figure styling
         ylabel('Position on Rod'); % y axis label
-        xlabel('Time'); % x axis label
+        xlabel('Time'); % x axis label 
+toc
+%% Forward Euler For-Loop
+% Heat Equation Solution using Forward Euler FEM
+% By Michael Klamkin 2017
+
+%% Constants
+dx = dx * 2; % pos step adjusted
+c = c * 1000; % diff. const. adjusted
+dt = 0.001; % time step
+gm = dt/((c)*(dx^2)); % a const for use in fwd euler
+
+%% Vectors
+x = 0:dx:1; % vect of steps of pos on rod
+t = 0:dt:7; % vect of steps of time
+
+%% Initial Conditions 
+row = length(t); % num time samples
+col = length(x); % num pos samples
+U = zeros(row,col); % build U
+for i = 1:length(x) %init cond
+    U(1,i) = 10*sin(pi*x(i));
+end
+
+%% for Loop + Plot
+for i = 1:row-1
+    for j = 2:col-1
+        U(i+1,j) = gm*(U(i,j+1) + U(i,j-1)) + (1-2*gm)*(U(i,j));
+    end
+end
+
+U(:,1) = []; U(:, end) = []; %shave 0-set rows/column
+
+figure(1);
+     subplot(1, 2, 2);
+     fig2 = pcolor(fliplr(u')); shading interp; %colormap jet %set(gca,'Ydir','reverse'); %pcolor allows for interp shading style
+        cbar2 = colorbar;
+        cbarpos2 = get(get(cbar2,'YLabel'),'Position');
+        set(get(cbar2,'YLabel'),'Position',[cbarpos2(1) + 3, cbarpos2(2) + 40, cbarpos2(3)],'Rotation',-90, 'FontSize', 15);
+        title('1D Heat Equation - Fwd Euler'); % figure title
+        set(gca, 'FontName', 'Times New Roman'); set(gca, 'TitleFontSizeMultiplier', 1.25); set(gca, 'FontSize', 15); % figure styling
+        ylabel('Position on Rod'); % y axis label
+        xlabel('Time'); % x axis label 
